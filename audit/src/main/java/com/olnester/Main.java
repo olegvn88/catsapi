10a8497ffcd89f6cccfb7dabdff1d895fa07aa15
Oleg Nesterov <oleg.nesterov@quantrs.ch>
2025-01-14 16:23:10 +0200

    trigger run tests after the merge

--- audit/src/main/java/com/olnester/Main.java
+++ audit/src/main/java/com/olnester/Main.java
@@ -3,5 +3,9 @@ package com.olnester;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world !");
+        String a = "hello";
+        String b = new String("hello");
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
     }
 }
