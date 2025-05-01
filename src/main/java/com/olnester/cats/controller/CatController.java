98e5044b34242058e45240a85dcde7086ff68a6f
Oleg Nesterov <oleg716@gmail.com>
2025-05-01 08:49:18 +0300

    set cron schedule

--- src/main/java/com/olnester/cats/controller/CatController.java
+++ src/main/java/com/olnester/cats/controller/CatController.java
@@ -3,6 +3,9 @@ package com.olnester.cats.controller;
 import com.olnester.cats.dto.CatDto;
 import com.olnester.cats.entity.CatEntity;
 import com.olnester.cats.service.CatService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.Enumeration;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/cats")
 public class CatController {
 
+    private static final Logger log = LoggerFactory.getLogger(CatController.class);
     private final CatService catService;
 
     public CatController(CatService catService) {
@@ -30,7 +35,14 @@ public class CatController {
     }
 
     @PostMapping
-    public ResponseEntity<CatEntity> addCat(@RequestBody CatDto cat) {
+    public ResponseEntity<CatEntity> addCat(@RequestBody CatDto cat,
+                                            HttpServletRequest request) {
+         Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            System.out.println(headerName + ": " + headerValue);
+        }
         return ResponseEntity.created(URI.create("")).body(catService.saveCat(cat));
     }
 
